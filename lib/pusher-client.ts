29bd8d98284cb5e259c49800b983a584a76b0387
Mathias Lindgaard <mathiaslindgaard@MacBook-Air-tilhrende-Mathias.local>
2026-03-27 20:50:12 +0100

    feat: add Pusher server and browser client singletons

---
+++ lib/pusher-client.ts
@@ -0,0 +1,12 @@
+import Pusher from 'pusher-js'
+
+let instance: Pusher | null = null
+
+export function getPusherClient(): Pusher {
+  if (!instance) {
+    instance = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
+      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    })
+  }
+  return instance
+}
